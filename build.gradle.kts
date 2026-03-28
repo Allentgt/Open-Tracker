f8a0904cfa0dd0f6f02da097305e854d921f0137
Allentgt <sabya93@gmail.com>
2026-03-28 12:46:11 +0530

    Fix GitHub Actions - use Gradle 8.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+// AndroidX is configured in gradle.properties
+
 plugins {
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
